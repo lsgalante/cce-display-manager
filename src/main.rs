706d679f89044fe2d1907f5234bc040e9a7426a5
Lucas Galante <lsgalante12@gmail.com>
2026-06-02 13:25:46 -0400

    Update system configuration and interface modules

--- src/main.rs
+++ src/main.rs
@@ -1182,6 +1182,8 @@ impl AppState {
             _ => {
                 if let Some(ref text) = event.utf8 {
                     Key::Character(text.clone())
+                } else if let Some(ch) = event.keysym.key_char() {
+                    Key::Character(ch.to_string())
                 } else {
                     return;
                 }
@@ -1334,7 +1336,11 @@ impl KeyboardHandler for AppState {
         _keyboard: &wl_keyboard::WlKeyboard,
         _surface: &wl_surface::WlSurface,
         _serial: u32,
-    ) {}
+    ) {
+        self.pressed_key = None;
+        self.ctrl_pressed = false;
+        self.shift_pressed = false;
+    }
 
     fn press_key(
         &mut self,
